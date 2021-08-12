890e9561b1a0d7b5f5aa46dfb327b1dcd4c28162
Alfonso Pimentel <alfonsopimentel97@gmail.com>
2021-08-11 18:57:45 -0700

    Initial Commit

---
+++ Poop Tracker/Poop_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poop_TrackerApp.swift
+//  Poop Tracker
+//
+//  Created by Alfonso Pimentel on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Poop_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
